e740d5829deb2f6504414882bb68b521a6260a33
WooYoung Cho <wy1.cho@samsung.com>
2019-07-30 12:15:40 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,1 @@
-module.exports = { "env": { "browser": true, "es6": true }, "extends": "eslint:recommended" };
+module.exports = { "env": { "browser": true, "es6": true }, "extends": "eslint:recommended-!!!!!!" };
